6e8398fd01b30fff38227e96058dfb9924dbc5c4
Sebastian Johnsson <sebastian.johnsson@gmail.com>
2015-11-24 23:06:29 +0700

    Release 0.3.4

--- lib/loopiator.rb
+++ lib/loopiator.rb
@@ -10,7 +10,7 @@ require File.join(File.dirname(__FILE__), 'loopiator/credits')
 require File.join(File.dirname(__FILE__), 'loopiator/client')
 
 module Loopiator
-  VERSION     =   "0.3.3"
+  VERSION     =   "0.3.4"
   
   class << self
     attr_writer :configuration
