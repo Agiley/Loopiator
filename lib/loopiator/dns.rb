ac32b3b53cebf53373914c8538a36a0d44570eac
Sebastian <sebastian.johnsson@gmail.com>
2018-08-14 12:27:37 +0200

    Add support for raising rate limit error for getZoneRecords as well

--- lib/loopiator/dns.rb
+++ lib/loopiator/dns.rb
@@ -2,23 +2,29 @@ module Loopiator
   module Dns
         
     def get_zone_records(domain, subdomain, customer_number: "")
-      records       =   []
-      response      =   parse_status_response(call("getZoneRecords", customer_number, encode_domain(domain), subdomain))
+      records         =   []
+      response        =   call("getZoneRecords", customer_number, encode_domain(domain), subdomain)
       
-      response.each do |item|
-        records    <<   Loopiator::Models::DnsRecord.new(item)
-      end if response && response.is_a?(Array)
+      if response && response.is_a?(Array) && response.any?
+        if response.first.is_a?(String) && response.first.downcase.to_sym.eql?(:rate_limited)
+          raise Loopiator::RateLimitError
+        else
+          response.each do |item|
+            records  <<   Loopiator::Models::DnsRecord.new(item)
+          end
+        end
+      end
       
       return records
     end
     
     def add_zone_record(domain, subdomain, record_obj, customer_number: "")
-      response      =   parse_status_response(call("addZoneRecord", customer_number, encode_domain(domain), subdomain, record_obj))
+      response        =   parse_status_response(call("addZoneRecord", customer_number, encode_domain(domain), subdomain, record_obj))
       return response.eql?(:ok)
     end
     
     def remove_zone_record(domain, subdomain, record_id, customer_number: "")
-      response      =   parse_status_response(call("removeZoneRecord", customer_number, encode_domain(domain), subdomain, record_id))
+      response        =   parse_status_response(call("removeZoneRecord", customer_number, encode_domain(domain), subdomain, record_id))
       return response.eql?(:ok)
     end
     
